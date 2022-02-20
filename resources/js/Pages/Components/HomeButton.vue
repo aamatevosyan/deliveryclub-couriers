787c5ae228312508c3979114c268cf4af1bde791
zilzilok <tnsafin@edu.hse.ru>
2022-02-20 16:12:35 +0300

    added home page with links #DCHSE-17

---
+++ resources/js/Pages/Components/HomeButton.vue
@@ -0,0 +1,29 @@
+<!-- prettier-ignore -->
+<template>
+  <div class="inline-flex rounded-md shadow">
+    <a
+      :href="href"
+      class="px-5 py-3 font-bold text-center rounded-md select-none"
+    >
+      {{ text }}
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeButton',
+  props: {
+    text: {
+      type: String,
+      default: 'Тык',
+    },
+    href: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped></style>
