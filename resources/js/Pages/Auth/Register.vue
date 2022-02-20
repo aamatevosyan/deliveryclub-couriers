665607035a2568793ba48e66224989227fb65cc9
zilzilok <tnsafin@edu.hse.ru>
2022-02-20 19:56:35 +0300

    add first reg page #DCHSE-17

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,8 +1,62 @@
-<template />
+<!-- prettier-ignore -->
+<template>
+  <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="max-w-md w-full space-y-8">
+      <div>
+        <p class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Зарегистрируйте аккаунт
+        </p>
+        <p class="mt-2 text-center text-sm text-gray-600">
+          Или
+          <a
+            :href="authRoute"
+            class="font-medium text-green-600 hover:text-green-500"
+          >войдите</a>
+          в существующий
+        </p>
+      </div>
+      <form
+        class="mt-8 space-y-6"
+        method="POST"
+        :action="regRoute"
+      >
+        <div>
+          <div>
+            <RegInput
+              type="email"
+              placeholder="aboba@example.com"
+              label="Введите почту"
+            />
+            <button
+              type="submit"
+              class="mt-5 group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-500 hover:bg-green-600"
+            >
+              Продолжить
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
 
 <script>
+import { stardust } from '@eidellev/adonis-stardust'
+import RegInput from '../Components/RegInput'
+
 export default {
   name: 'Register',
+  components: {
+    RegInput,
+  },
+  computed: {
+    authRoute() {
+      return stardust.route('auth.login.show')
+    },
+    regRoute() {
+      return stardust.route('auth.register.store')
+    },
+  },
 }
 </script>
 
---
+++ resources/js/Pages/Components/RegInput.vue
@@ -0,0 +1,33 @@
+<!-- prettier-ignore -->
+<template>
+  <label class="block">
+    <span class="text-gray-700">{{ label }}:</span>
+    <input
+      :type="type"
+      :placeholder="placeholder"
+      class="mt-1 rounded-md relative block w-full px-3 py-2 border border-gray-300 text-gray-900 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+    >
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'RegInput',
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      default: 'Пук',
+    },
+    label: {
+      type: String,
+      default: 'Пук',
+    },
+  },
+}
+</script>
+
+<style scoped></style>
